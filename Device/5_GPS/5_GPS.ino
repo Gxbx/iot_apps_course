9337ba3806167e6399f778e33fc93f19102926be
Gxbx <gado.dev@outlook.com>
2019-09-05 17:49:59 -0500

    fix lat and lng encode

--- Device/5_GPS/5_GPS.ino
+++ Device/5_GPS/5_GPS.ino
@@ -12,22 +12,43 @@
 #endif
 #include <WiFiManager.h> 
 #include <HTTPClient.h>   
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+
+static const int RXPin = 16, TXPin = 17;
 
 HTTPClient http;
+TinyGPSPlus gps;
+SoftwareSerial SerialGPS(RXPin,TXPin);
 
 void setup() {
     Serial.begin(115200);
+    SerialGPS.begin(115200);
     WiFiManager wifiManager;
     wifiManager.autoConnect("GaboESP32");
     Serial.println("Estoy connectado a la red :D");
 }
 
 void loop() {  
-    http.begin("https://iot-apps-course.firebaseio.com/data.json");
+
+    http.begin("https://iot-apps-course.firebaseio.com/position.json");
     http.addHeader("Content-Type","text/plain");
  
-    String payload = "{\"Temperature\":" + String(temp) + ", \"Humidity\": " + String(hum)+"}";
-    int httpCode = http.POST(payload);
-    Serial.println(httpCode);
-    Serial.println(payload);
+    while (SerialGPS.available())
+    {
+        gps.encode(SerialGPS.read());
+        if (gps.location.isUpdated())
+        {
+            Serial.print ("Latitude= ");
+            Serial.print (gps.location.lat(), 6);
+            Serial.print ("Latitude= ");
+            Serial.print (gps.location.lng(), 6);      
+            String payload = "{\"Lat\":" + String(gps.location.lat()) + ", \"Lng\": " + String(gps.location.lat())+"}";
+            int httpCode = http.POST(payload);
+            Serial.println(httpCode);
+            Serial.println(payload);
+        }
+        
+    }
+
 }
